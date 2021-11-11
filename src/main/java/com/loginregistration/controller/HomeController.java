f4066eebdd66eea0a9b0efa5afa1272c8775d7aa
Kshitiz Jain <coolkshitiz786@gmail.com>
2021-11-11 19:08:41 +0530

    removed used imports

--- src/main/java/com/loginregistration/controller/HomeController.java
+++ src/main/java/com/loginregistration/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.loginregistration.controller;
 
-import java.net.http.HttpRequest;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -10,14 +8,8 @@ import com.loginregistration.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
--- src/main/java/com/loginregistration/controller/LoginController.java
+++ src/main/java/com/loginregistration/controller/LoginController.java
@@ -13,11 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-// import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
